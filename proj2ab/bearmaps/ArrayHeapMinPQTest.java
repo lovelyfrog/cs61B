71a1fc7b7c437251f006f82d96a024ef183bcacd
lovelyfrog <15307110114@fudan.edu.cn>
2020-03-03 21:30:54 +0800

    proj2a finished

--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,51 @@
 package bearmaps;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
+    @Test
+    public void testAdd() {
+        ArrayHeapMinPQ<Integer> heap = new ArrayHeapMinPQ<>();
+        for (int i=1; i<6; i++) {
+            heap.add(6-i, 6-i);
+        }
+        assertEquals(1, (int) heap.getSmallest());
+    }
+
+    @Test
+    public void testContains() {
+        ArrayHeapMinPQ<Integer> heap = new ArrayHeapMinPQ<>();
+        for (int i=1; i<6; i++) {
+            heap.add(6-i, 6-i);
+        }
+        assertEquals(1, (int) heap.getSmallest());
+        assertTrue(heap.contains(1));
+        assertFalse(heap.contains(6));
+    }
+
+    @Test
+    public void testRemoveSmallest() {
+        ArrayHeapMinPQ<Integer> heap = new ArrayHeapMinPQ<>();
+        for (int i=1; i<6; i++) {
+            heap.add(6-i, 6-i);
+        }
+        heap.removeSmallest();
+        assertEquals(2, (int) heap.getSmallest());
+    }
+
+    @Test
+    public void testChangePriority() {
+        ArrayHeapMinPQ<Integer> heap = new ArrayHeapMinPQ<>();
+        for (int i=1; i<6; i++) {
+            heap.add(6-i, 6-i);
+        }
+        heap.changePriority(2, 0);
+        assertEquals(2, (int) heap.getSmallest());
+        heap.changePriority(1, 6);
+        heap.removeSmallest();
+        assertEquals(3, (int) heap.getSmallest());
+    }
+
+
 }
