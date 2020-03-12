a4f44781c9eae485a457d0361ea0203ba458fd30
lovelyfrog <15307110114@fudan.edu.cn>
2020-03-12 16:01:21 +0800

    clab11 finished

--- clab11/BnBSolver.java
+++ clab11/BnBSolver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -10,16 +11,36 @@ import java.util.List;
  */
 public class BnBSolver {
 
+    private List<Bear> bears;
+    private List<Bed> beds;
     public BnBSolver(List<Bear> bears, List<Bed> beds) {
         // TODO: Fix me.
+        this.bears = bears;
+        this.beds = beds;
     }
 
     /**
      * Returns List of Bears such that the ith Bear is the same size as the ith Bed of solvedBeds().
+     * we need to sort the bears and beds
      */
     public List<Bear> solvedBears() {
         // TODO: Fix me.
-        return null;
+//        quickSort(bears);
+        List<Bear> ans = new ArrayList<>();
+//        for (Bear tmpBear: bears) {
+//            int i = 0;
+//            for (Bed tmpBed: beds) {
+//                if (tmpBear.compareTo(tmpBed) == 0) {
+//                    ans.set(i, tmpBear);
+//                }
+//                i += 1;
+//            }
+//        }
+        for (Bed bed: beds) {
+            Bear tmp = new Bear(bed.getSize());
+            ans.add(tmp);
+        }
+        return ans;
     }
 
     /**
@@ -27,6 +48,46 @@ public class BnBSolver {
      */
     public List<Bed> solvedBeds() {
         // TODO: Fix me.
-        return null;
+//        quickSort(beds);
+        return beds;
+    }
+
+    /*
+     I misunderstood the meaning of the lab that I compare bears and begs separately
+     */
+
+    public <T extends Comparable> void quickSort(List<T> list) {
+        quickSortHelper(list, 0, list.size()-1);
+    }
+
+    public <T extends Comparable> void quickSortHelper(List<T> list, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int L = start + 1;
+        int G = end;
+        T pivot = list.get(start);
+        while (L <= G) {
+            if (list.get(L).compareTo(pivot) < 0) {
+                L += 1;
+                continue;
+            }
+            if (list.get(G).compareTo(pivot) > 0) {
+                G -= 1;
+                continue;
+            }
+            swap(list, L, G);
+            L += 1;
+            G -= 1;
+        }
+        swap(list, start, G);
+        quickSortHelper(list, start, G-1);
+        quickSortHelper(list, L, end);
+    }
+
+    public <T extends Comparable> void swap(List<T> list, int i, int j) {
+        T tmp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, tmp);
     }
 }
